cac3ebd8ba55a9099985c802f3c3115f193e2d34
pleung <pleung@foo>
2025-11-19 19:55:32 -0500

    save all

--- stacks/api_gateway/main.tf
+++ stacks/api_gateway/main.tf
@@ -20,26 +20,4 @@ module "api_gateway" {
     Environment = "dev"
     ManagedBy   = "Terraform"
   }
-}module "api_gateway" {
-  source = "../../modules/api_gateway"
-
-  environment     = "dev"
-  region          = var.region
-  project         = var.project
-  gateway_name    = var.gateway_name
-  apis            = var.apis # map(object) of API configs
-  stage_name      = var.stage_name
-  custom_domains  = var.custom_domains # optional map
-
-  providers = {
-    aws   = aws.dev
-    azure = azurerm.dev
-    gcp   = google.dev
-  }
-
-  tags = {
-    Owner       = "infra-team"
-    Environment = "dev"
-    ManagedBy   = "Terraform"
-  }
 }
--- stacks/dns/main.tf
+++ stacks/dns/main.tf
@@ -19,25 +19,4 @@ module "dns" {
     Environment = "dev"
     ManagedBy   = "Terraform"
   }
-}module "dns" {
-  source = "../../modules/dns"
-
-  environment     = "dev"
-  region          = var.region
-  project         = var.project
-  domain_name     = var.domain_name
-  zone_type       = var.zone_type # "public" or "private"
-  record_sets     = var.record_sets # map(object) of records
-
-  providers = {
-    aws   = aws.dev
-    azure = azurerm.dev
-    gcp   = google.dev
-  }
-
-  tags = {
-    Owner       = "infra-team"
-    Environment = "dev"
-    ManagedBy   = "Terraform"
-  }
 }
