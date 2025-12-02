40e8df1460ae14e005f70131c0e25ecf6ef54e69
pleung <pleung@foo>
2025-12-01 20:27:35 -0500

    fix SG

--- stacks/security_group/main.tf
+++ stacks/security_group/main.tf
@@ -5,20 +5,25 @@ module "security_group" {
   environment     = "dev"
   region          = each.value.region
   project         = each.value.project
-  group_name      = each.value.group_name
-  group_rules     = each.value.group_rules
-  vpc_id          = each.value.vpc_id
-  attach_to_roles = each.value.attach_to_roles # optional IAM integration
+  cloud           = each.value.cloud
+  name            = each.value.name
+  description     = each.value.description
+
+  # AWS
+  vpc_id = each.value.vpc_id
+
+  # Azure
+  resource_group = each.value.resource_group
+
+  # GCP
+  network = each.value.network
+  ingress = each.value.ingress
+  egress = each.value.egress
+  tags = each.value.tags
 
   providers = {
     aws   = aws.dev
     azure = azurerm.dev
     gcp   = google.dev
-  }
-
-  tags = {
-    Owner       = "infra-team"
-    Environment = "dev"
-    ManagedBy   = "Terraform"
   }
 }
